c5a5ba51beb40acbfc2af846282f8603a8136461
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-04 13:49:05 +0000

    24

--- 24.py
+++ 24.py
@@ -1,8 +1,8 @@
 f = open('24.txt').readline()
 max1 = 0
 k = 0
-for i in range(0, len(f), 2):
-  if (f[i-1] == 'A' and f[i] == 'B') or (f[i-1] == 'A' and f[i] == 'C'):
+for i in range(1, len(f), 2):
+  if (f[i] == 'A' and f[i+1] == 'B') or (f[i] == 'A' and f[i+1] == 'C'):
     k += 1
     if k > max1:
       max1 = k
@@ -14,8 +14,8 @@ print(max1) # 7
 f = open('24.txt').readline()
 max2 = 0
 k = 0
-for i in range(1, len(f), 2):
-  if (f[i-1] == 'C' and f[i] == 'B') or (f[i-1] == 'C' and f[i] == 'A'):
+for i in range(0, len(f), 2):
+  if (f[i] == 'C' and f[i+1] == 'B') or (f[i] == 'C' and f[i+1] == 'A'):
     k += 1
     if k > max2:
       max2 = k
