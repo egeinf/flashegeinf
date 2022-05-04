6e8ede23435c7dc8939282372e6db4e8e844457e
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-04 09:27:05 +0000

    15 fix

--- 15.py
+++ 15.py
@@ -1,22 +1,22 @@
-for A in range(70,91):
+for A in range(100):
   flag = 1
-  for x in range(1,100000):
+  for x in range(1,1000):
     flag *= ((x % 3 == 0) <= (x % 5 != 0)) or (x+A >= 90)
   if flag:
     print(A)
     break
 
-for A in range(70,91):
+for A in range(100):
   flag = 1
-  for x in range(1,100000):
+  for x in range(1,1000):
     flag *= ((x % 2 == 0) <= (x % 5 != 0)) or (x+A >= 80)
   if flag:
     print(A)
     break
 
-for A in range(50,58):
+for A in range(100):
   flag = 1
-  for x in range(1,100000):
+  for x in range(1,1000):
     flag *= ((x % 3 == 0) <= (x % 5 != 0)) or (x+A >= 70)
   if flag:
     print(A)
