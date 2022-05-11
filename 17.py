376bfefd7e8d8093f97c90418236371978279de5
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-11 11:07:46 +0000

    17.3

--- 17.py
+++ 17.py
@@ -34,3 +34,22 @@ for i in range(len(a)):
     k += 1
     pair.append(a[i-1] + a[i]) # Сумма элементов одной пары
 print(k, max(pair))
+
+##################################################################### 17.3
+f = open('17.txt')
+a = [int(i.strip()) for i in f]
+
+# Мин. элемент последовательности кратный 23
+ost17 = []
+for i in range(len(a)):
+  if a[i] % 17 == 0:
+    ost17.append(a[i])
+delitel = min(ost17)
+# Пары которые делятся на Мин. элемент последовательности кратный 23
+k = 0
+pair = []
+for i in range(len(a)):
+  if (a[i-1] % delitel == 0) or (a[i] % delitel == 0):
+    k += 1
+    pair.append(a[i-1] + a[i]) # Сумма элементов одной пары
+print(k, max(pair))
