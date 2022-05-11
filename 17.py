f47ec1956a2a4bc3a2a25c7dc0c94558e3aa0e78
voron5096 <70198995+voron5096@users.noreply.github.com>
2022-05-11 11:04:55 +0000

    17 excel & python

---
+++ 17.py
@@ -0,0 +1,36 @@
+####### Решение Excel: drive.google.com/drive/folders/1Hg_qDNTp30HBleRp4l0wzJ95IdjdAqdv
+f = open('17.txt')
+a = [int(i.strip()) for i in f]
+
+# Мин. элемент последовательности кратный 23
+ost23 = []
+for i in range(len(a)):
+  if a[i] % 23 == 0:
+    ost23.append(a[i])
+delitel = min(ost23)
+# Пары которые делятся на Мин. элемент последовательности кратный 23
+k = 0
+pair = []
+for i in range(len(a)):
+  if (a[i-1] % delitel == 0) or (a[i] % delitel == 0):
+    k += 1
+    pair.append(a[i-1] + a[i]) # Сумма элементов одной пары
+print(k, max(pair))
+##################################################################### 17.2
+f = open('17.txt')
+a = [int(i.strip()) for i in f]
+
+# Мин. элемент последовательности кратный 23
+ost4 = []
+for i in range(len(a)):
+  if a[i] % 4 == 0:
+    ost4.append(a[i])
+delitel = min(ost4)
+# Пары которые делятся на Мин. элемент последовательности кратный 23
+k = 0
+pair = []
+for i in range(len(a)):
+  if (a[i-1] % delitel == 0) or (a[i] % delitel == 0):
+    k += 1
+    pair.append(a[i-1] + a[i]) # Сумма элементов одной пары
+print(k, max(pair))
